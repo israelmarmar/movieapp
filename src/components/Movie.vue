ed22687ab4f2a69e5f7c0e263a638a836cfb94f0
israelmarmar <email.israel.br@gmail.com>
2019-09-14 12:10:31 -0400

    first commit

---
+++ src/components/Movie.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h2>welcome to single movie component</h2>
+          <div>{{singleMovie}}</div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  props: ['id'],
+  data () {
+    return {
+      singleMovie: ''
+    }
+  },
+  mounted () {
+    axios
+      .get('http://www.omdbapi.com/?apikey=293df754&i='+this.id+'&Content-Type=application/json')
+      .then(response => {
+        console.log("oimovie")
+        this.singleMovie = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+<style>
+</style>
